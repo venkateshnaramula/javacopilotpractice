7abcc68eefc69f321d801de8f929e888cd60139e
venkateshnaramula <155065974+venkateshnaramula@users.noreply.github.com>
2023-12-29 09:27:28 +0000

    message

--- t2.java
+++ t2.java
@@ -1,10 +1,17 @@
 import java.util.Arrays;
 
-public class SortingExample {
+public class t2 {
     public static void main(String[] args) {
         int[] numbers = {5, 2, 8, 1, 7};
 
         // Task: Use Copilot to complete the code to sort the array in ascending order
         // TODO: Complete the code here
+        Arrays.sort(numbers);
+
+        // Print the sorted array
+        System.out.println("Sorted Array in Ascending Order:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
     }
+}
 }
--- t3.java
+++ t3.java
@@ -1,1 +1,5 @@
-
+public class t3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
