cc9272d3ca4cf08eb2ff144d4e63464e58f21dcf
venkateshnaramula <155065974+venkateshnaramula@users.noreply.github.com>
2023-12-29 09:02:22 +0000

    Initial commit

---
+++ t1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListProcessor {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("orange");
+
+        // Task: Use Copilot to complete the code to print each word in uppercase
+        for (String word : words) {
+            // TODO: Complete the code here
+        }
+    }
+}
---
+++ t2.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class SortingExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 7};
+
+        // Task: Use Copilot to complete the code to sort the array in ascending order
+        // TODO: Complete the code here
+    }
+}
